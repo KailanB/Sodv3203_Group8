70257675e69e70121ccbe8e11fa325a242cc71c1
Deepanshi <deepanshi20ac@gmail.com>
2025-04-03 17:29:23 -0600

    include accept and delete function

--- App/app/src/main/java/com/example/skillswapapp/data/repository/OfflineFriendshipRepository.kt
+++ App/app/src/main/java/com/example/skillswapapp/data/repository/OfflineFriendshipRepository.kt
@@ -4,6 +4,7 @@ import com.example.skillswapapp.data.dao.FriendshipDao
 import com.example.skillswapapp.data.entities.Friendship
 import com.example.skillswapapp.data.entities.relations.UserFriendList
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 
 data class OfflineFriendshipRepository(
     private val friendshipDao: FriendshipDao
@@ -15,5 +16,24 @@ data class OfflineFriendshipRepository(
 
     override fun getAllFriendshipsByIdStream(id:Int): Flow<List<UserFriendList>> = friendshipDao.getAllFriendshipsById(id)
     override fun getFriendshipStream(id:Int): Flow<Friendship?> = friendshipDao.getFriendship(id)
+    override suspend fun acceptFriendRequest(userId: Int, friendId: Int) {
+        // Collect the friendship data, first() is used to collect the first element emitted by the flow
+        val friendship = friendshipDao.getFriendshipByUserIdAndFriendId(userId, friendId).first()
+
+        // Check if exist or not
+        if (friendship != null) {
+            val updatedFriendship = friendship.copy(status = "accepted")
+            friendshipDao.update(updatedFriendship)  // Update status in the database
+        }
+    }
+
+    override suspend fun deleteFriend(userId: Int, friendId: Int) {
+        val friendship = friendshipDao.getFriendshipByUserIdAndFriendId(userId, friendId).first()
+
+        // Check if exist or not
+        if (friendship != null) {
+            friendshipDao.delete(friendship)  // Deleted from database
+        }
+    }
 
 }
