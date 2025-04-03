aad7adfacdc5c699dee2f9cb9237387fdd0edfad
Deepanshi <deepanshi20ac@gmail.com>
2025-04-03 17:33:09 -0600

    make changes in composable("friends")
    when i click the friend option it is closing the application maybe due to userId. I am not sure

--- App/app/src/main/java/com/example/skillswapapp/SkillSwapScreen.kt
+++ App/app/src/main/java/com/example/skillswapapp/SkillSwapScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -23,10 +25,12 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.skillswapapp.data.dao.FriendshipDao
 import com.example.skillswapapp.view.FriendsScreen
 import com.example.skillswapapp.view.HomeScreen
 import com.example.skillswapapp.view.ProfileScreen
 import com.example.skillswapapp.view.SwapsScreen
+import com.example.skillswapapp.viewModel.FriendsViewModel
 import com.example.skillswapapp.viewModel.UsersViewModel
 
 
@@ -108,8 +112,15 @@ fun SkillSwapApp(
                     modifier = Modifier
                 )
             }
-            composable(route = "friends") {
+
+            composable(route = "friends?user_id={user_id}") { backStackEntry ->
+                val userId = backStackEntry.arguments?.getString("user_id")?.toInt() ?: 0
+                val friendsViewModel: FriendsViewModel = viewModel()
+
+                // Pass the necessary parameters to FriendsScreen
                 FriendsScreen(
+                    viewModel = friendsViewModel,
+                    userId = userId,
                     modifier = Modifier
                 )
             }
