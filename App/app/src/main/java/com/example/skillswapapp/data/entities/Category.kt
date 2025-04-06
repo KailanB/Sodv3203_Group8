96a6261f239d953f943632b05152b77fa940b7b4
KailanB <kailanbates@gmail.com>
2025-04-05 20:37:42 -0600

    added unique constraint to category name

--- App/app/src/main/java/com/example/skillswapapp/data/entities/Category.kt
+++ App/app/src/main/java/com/example/skillswapapp/data/entities/Category.kt
@@ -1,9 +1,13 @@
 package com.example.skillswapapp.data.entities
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "category")
+@Entity(
+    tableName = "category",
+    indices = [Index(value = ["category"], unique = true)]
+)
 data class Category(
     @PrimaryKey(autoGenerate = true)
     val category_id: Int,
