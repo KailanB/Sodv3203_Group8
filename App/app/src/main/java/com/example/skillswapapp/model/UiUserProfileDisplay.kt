387b094d1e337ae95c06549fa58d6c40b184de26
KailanB <kailanbates@gmail.com>
2025-04-10 15:37:06 -0600

    profile display class is slightly different as it also contains password info

---
+++ App/app/src/main/java/com/example/skillswapapp/model/UiUserProfileDisplay.kt
@@ -0,0 +1,12 @@
+package com.example.skillswapapp.model
+
+import com.example.skillswapapp.data.entities.User
+import com.example.skillswapapp.data.relations.UserSeeksSkillsDetails
+import com.example.skillswapapp.data.relations.UserSkillDetails
+
+class UiUserProfileDisplay (
+    val user : User,
+
+    val skills: List<UserSkillDetails>,
+    val seeksSkills: List<UserSeeksSkillsDetails>
+)
