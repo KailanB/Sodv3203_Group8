7235e435ca21c1a72bb8e12bb55c0999e83306dd
KailanB <kailanbates@gmail.com>
2025-04-10 15:34:39 -0600

    added more repo implementations

--- App/app/src/main/java/com/example/skillswapapp/data/AppContainer.kt
+++ App/app/src/main/java/com/example/skillswapapp/data/AppContainer.kt
@@ -3,22 +3,30 @@ package com.example.skillswapapp.data
 import android.content.Context
 import com.example.skillswapapp.data.database.DatabaseInitializer
 import com.example.skillswapapp.data.database.SkillSwapDatabase
-import com.example.skillswapapp.data.repository.CategoryRepository
+import com.example.skillswapapp.data.repository.iRepositories.CategoryRepository
+import com.example.skillswapapp.data.repository.iRepositories.LocationRepository
 import com.example.skillswapapp.data.repository.OfflineCategoryRepository
+import com.example.skillswapapp.data.repository.OfflineLocationRepository
 import com.example.skillswapapp.data.repository.OfflineSkillRepository
 import com.example.skillswapapp.data.repository.OfflineUserRepository
-import com.example.skillswapapp.data.repository.SkillRepository
-import com.example.skillswapapp.data.repository.UserRepository
+import com.example.skillswapapp.data.repository.OfflineUserSkillsRepository
+import com.example.skillswapapp.data.repository.OfflineUserSeeksSkillsRepository
+import com.example.skillswapapp.data.repository.iRepositories.SkillRepository
+import com.example.skillswapapp.data.repository.iRepositories.UserRepository
+import com.example.skillswapapp.data.repository.iRepositories.UserSeeksSkillsRepository
+import com.example.skillswapapp.data.repository.iRepositories.UserSkillsRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 
 interface AppContainer {
+    val userRepository: UserRepository
+    val userSkillRepository: UserSkillsRepository
+    val userSeeksSkillsRepository: UserSeeksSkillsRepository
     val categoryRepository: CategoryRepository
     val skillRepository: SkillRepository
-    val userRepository: UserRepository
+    val locationRepository: LocationRepository
 }
 
 
@@ -38,12 +46,24 @@ class AppDataContainer(private val context: Context) : AppContainer {
         OfflineUserRepository(database.userDao())
     }
 
+    override val userSkillRepository: UserSkillsRepository by lazy {
+        OfflineUserSkillsRepository(database.userSkillsDao())
+    }
+
+    override val userSeeksSkillsRepository: UserSeeksSkillsRepository by lazy {
+        OfflineUserSeeksSkillsRepository(database.userSeeksSkillsDao())
+    }
+
     override val categoryRepository: CategoryRepository by lazy {
         OfflineCategoryRepository(database.categoryDao())
     }
 
     override val skillRepository: SkillRepository by lazy {
         OfflineSkillRepository(database.skillDao())
+    }
+
+    override val locationRepository: LocationRepository by lazy {
+        OfflineLocationRepository(database.locationDao())
     }
 
 }
