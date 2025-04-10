51d5fe96df87b197165e40b24889fe8646b6690d
KailanB <kailanbates@gmail.com>
2025-04-10 15:37:28 -0600

    changed userWithSkills class to use the Details class that holds name as well as entity class of skill / skill seeking

--- App/app/src/main/java/com/example/skillswapapp/model/UserWithSkills.kt
+++ App/app/src/main/java/com/example/skillswapapp/model/UserWithSkills.kt
@@ -2,13 +2,15 @@ package com.example.skillswapapp.model
 
 
 import com.example.skillswapapp.data.entities.Skill
+import com.example.skillswapapp.data.relations.UserSeeksSkillsDetails
+import com.example.skillswapapp.data.relations.UserSkillDetails
 import com.example.skillswapapp.data.relations.UserWithoutSecureInfo
 
 
 data class UserWithSkills(
     val user : UserWithoutSecureInfo,
 
-    val skills: List<Skill>,
-    val seeksSkills: List<Skill>
+    val skills: List<UserSkillDetails>,
+    val seeksSkills: List<UserSeeksSkillsDetails>
 )
 
