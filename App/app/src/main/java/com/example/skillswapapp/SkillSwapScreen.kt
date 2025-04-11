d82d68c99bc5c9c70ffe1b173b881a4f574b0792
KailanB <kailanbates@gmail.com>
2025-04-11 07:54:08 -0600

    UI adjustments to Icons, added viewUserProfileScreen to nav

--- App/app/src/main/java/com/example/skillswapapp/SkillSwapScreen.kt
+++ App/app/src/main/java/com/example/skillswapapp/SkillSwapScreen.kt
@@ -4,7 +4,9 @@ package com.example.skillswapapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Done
@@ -15,6 +17,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -26,14 +29,18 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.skillswapapp.view.FriendsScreen
 import com.example.skillswapapp.view.HomeScreen
 import com.example.skillswapapp.view.ProfileScreen
 import com.example.skillswapapp.view.SwapsScreen
+import com.example.skillswapapp.view.UserDetailsDestination
 import com.example.skillswapapp.view.UserEntryScreen
+import com.example.skillswapapp.view.ViewUserProfileScreen
 import com.example.skillswapappimport.SessionViewModel
 
 
@@ -52,17 +59,50 @@ fun SkillSwapNavBar(
                 horizontalArrangement = Arrangement.SpaceBetween
             )
             {
-                IconButton(onClick = { navController.navigate("home") }) {
-                    Icon(Icons.Default.Home, contentDescription = "Home")
+                IconButton(
+                    onClick = { navController.navigate("home") }
+                ) {
+                    Icon(
+                        Icons.Default.Home,
+                        contentDescription = "Home",
+                        modifier = Modifier.height(75.dp)
+                            .width(75.dp),
+                        tint = MaterialTheme.colorScheme.primaryContainer
+                    )
                 }
-                IconButton(onClick = { navController.navigate("profile") }) {
-                    Icon(Icons.Default.AccountCircle, contentDescription = "Profile")
+
+                IconButton(
+                    onClick = { navController.navigate("profile") }
+                ) {
+                    Icon(
+                        Icons.Default.AccountCircle,
+                        contentDescription = "Profile",
+                        modifier = Modifier.height(75.dp)
+                            .width(75.dp),
+                        tint = MaterialTheme.colorScheme.primaryContainer
+                    )
                 }
-                IconButton(onClick = { navController.navigate("swaps") }) {
-                    Icon(Icons.Default.Done, contentDescription = "Swaps")
+                IconButton(
+                    onClick = { navController.navigate("swaps") }
+                ) {
+                    Icon(
+                        Icons.Default.Done,
+                        contentDescription = "Swaps",
+                        modifier = Modifier.height(75.dp)
+                            .width(75.dp),
+                        tint = MaterialTheme.colorScheme.primaryContainer
+                    )
                 }
-                IconButton(onClick = { navController.navigate("friends/1") }) {
-                    Icon(Icons.Default.Person, contentDescription = "Friends")
+                IconButton(
+                    onClick = { navController.navigate("friends/1") }
+                ) {
+                    Icon(
+                        Icons.Default.Person,
+                        contentDescription = "Friends",
+                        modifier = Modifier.height(75.dp)
+                            .width(75.dp),
+                        tint = MaterialTheme.colorScheme.primaryContainer
+                    )
                 }
             }
 
@@ -123,23 +163,39 @@ fun SkillSwapApp(
         ) {
             composable(route = "home") {
                 HomeScreen(
+
                     sessionViewModel = sessionViewModel,
+                    navigateToViewUserProfile = {
+                        navController.navigate("${UserDetailsDestination.route}/${it}")
+                    },
                     modifier = Modifier
+                )
+            }
+            composable(
+                route = UserDetailsDestination.routeWithArgs,
+                arguments = listOf(navArgument("userId") {
+                    type = NavType.IntType
+                })
+
+            ) {
+                ViewUserProfileScreen(
+                    sessionViewModel = sessionViewModel
                 )
             }
             composable(route = "profile") {
                 ProfileScreen(
                     navigateToEditUser = {navController.navigate(route = "userEntryScreen")},
-                    sessionViewModel = sessionViewModel,
-                    modifier = Modifier
+                    sessionViewModel = sessionViewModel
                 )
             }
             composable(route = "userEntryScreen"){
                 val currentUser = sessionViewModel.currentUser.collectAsState()
                 UserEntryScreen(
-                    currentUser = currentUser.value?.user
+                    currentUser = currentUser.value
                 )
             }
+
+
 
             composable(route = "swaps") {
                 SwapsScreen(
