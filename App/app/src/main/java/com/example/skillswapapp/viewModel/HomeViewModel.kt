8ea9c1f03a14964bbfda3c3b061413eef2831efd
KailanB <kailanbates@gmail.com>
2025-04-11 07:57:52 -0600

    updated userSkills display for reusability and UI friendly / repo friendly classes

--- App/app/src/main/java/com/example/skillswapapp/viewModel/HomeViewModel.kt
+++ App/app/src/main/java/com/example/skillswapapp/viewModel/HomeViewModel.kt
@@ -3,43 +3,73 @@ package com.example.skillswapapp.viewModel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.skillswapapp.data.repository.iRepositories.CategoryRepository
+import com.example.skillswapapp.data.relations.UserSeeksSkillsDetails
+import com.example.skillswapapp.data.relations.UserSkillDetails
+import com.example.skillswapapp.data.repository.iRepositories.UserRepository
+import com.example.skillswapapp.data.repository.iRepositories.UserSeeksSkillsRepository
+import com.example.skillswapapp.data.repository.iRepositories.UserSkillsRepository
+import com.example.skillswapapp.model.UiDisplaySkill
+import com.example.skillswapapp.model.UiUserDisplay
 import com.example.skillswapapp.state.HomeUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class HomeViewModel (
-    private val categoryRepository: CategoryRepository
-) : ViewModel() {
+    private val userRepository: UserRepository,
+    private val userSkillsRepository: UserSkillsRepository,
+    private val userSeeksSkillsRepository: UserSeeksSkillsRepository
+): ViewModel() {
 
     private val _homeUiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
     val homeUiState: StateFlow<HomeUiState> = _homeUiState
 
+
     init {
-        getAllCategories()
+        getAllUsers()
     }
 
-    fun getAllCategories() {
+    fun getAllUsers() {
         _homeUiState.value = HomeUiState.Loading
         viewModelScope.launch {
             try {
-                categoryRepository.getAllCategoryStream().map { HomeUiState.Success(it) }
-                    .collect { uiState ->
-                        _homeUiState.value = uiState
-                    }
+                val usersFlow = userRepository.getAllUsersStream()
 
+                usersFlow.collect{userList ->
+                    val usersWithSkills = userList.map { user ->
+                        val skills =
+                            userSkillsRepository.getAllUserSkillsByIdStream(user.user_id).first()
+                        val seeksSkills =
+                            userSeeksSkillsRepository.getAllUserSeeksSkillsByIdStream(user.user_id)
+                                .first()
+
+                        val uiSkills = skills.map { it.toUiDisplaySkill() }
+                        val uiSkillsSeeking = seeksSkills.map { it.toUiDisplaySkill() }
+                            UiUserDisplay(user, uiSkills, uiSkillsSeeking)
+                        }
+                        _homeUiState.value = HomeUiState.Success(users = usersWithSkills)
+                }
 
             } catch (exception: Exception) {
                 Log.d("ERROR123", "getAllCategories: ERROR")
                 _homeUiState.value = HomeUiState.Error
             }
         }
-
     }
 
-    companion object {
-        private const val TIMEOUT_MILLIS = 5_000L
+    fun UserSkillDetails.toUiDisplaySkill(): UiDisplaySkill {
+        return UiDisplaySkill(
+            skillId = userSkills.skill_id,
+            skillName = skill_name,
+            description = userSkills.skill_description
+        )
+    }
+    fun UserSeeksSkillsDetails.toUiDisplaySkill(): UiDisplaySkill {
+        return UiDisplaySkill(
+            skillId = userSeeksSkills.skill_id,
+            skillName = skill_name,
+            description = userSeeksSkills.skill_seekers_description
+        )
     }
 }
