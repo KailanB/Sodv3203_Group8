2ce73c8a528ba3b4f0ae9770ec9ede199e81e5b0
KailanB <kailanbates@gmail.com>
2025-04-11 07:53:39 -0600

    updated user session as combined flow so that data updates responsively

--- App/app/src/main/java/com/example/skillswapapp/SessionViewModel.kt
+++ App/app/src/main/java/com/example/skillswapapp/SessionViewModel.kt
@@ -3,13 +3,17 @@ package com.example.skillswapappimport
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.skillswapapp.data.entities.Location
+import com.example.skillswapapp.data.repository.iRepositories.LocationRepository
 import com.example.skillswapapp.data.repository.iRepositories.UserRepository
 import com.example.skillswapapp.data.repository.iRepositories.UserSeeksSkillsRepository
 import com.example.skillswapapp.data.repository.iRepositories.UserSkillsRepository
 import com.example.skillswapapp.model.UiUserProfileDisplay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
@@ -17,7 +21,8 @@ import kotlinx.coroutines.launch
 class SessionViewModel (
     private val userRepository: UserRepository,
     private val userSkillsRepository: UserSkillsRepository,
-    private val userSeeksSkillsRepository: UserSeeksSkillsRepository
+    private val userSeeksSkillsRepository: UserSeeksSkillsRepository,
+    private val locationRepository: LocationRepository
 ) : ViewModel() {
 
     private val _currentUser = MutableStateFlow<UiUserProfileDisplay?>(null)
@@ -31,18 +36,21 @@ class SessionViewModel (
         viewModelScope.launch {
             try {
                 // getting user via dummy value 1 since log in functionality is not implemented
-                userRepository.getUserAllInfoStream(1)
-                    .collect { user ->
-                        val skills = userSkillsRepository.getAllUserSkillsByIdStream(user.user_id).first()
-                        val seeksSkills = userSeeksSkillsRepository.getAllUserSeeksSkillsByIdStream(user.user_id).first()
+                        val userId = 1
+                        val userFlow = userRepository.getUserAllInfoStream(userId)
+                        val skillsFlow = userSkillsRepository.getAllUserSkillsByIdStream(userId)
+                        val seeksSkillsFlow = userSeeksSkillsRepository.getAllUserSeeksSkillsByIdStream(userId)
+                        val locationFlow = locationRepository.getUserLocationStream(userId)
 
-                        _currentUser.value = UiUserProfileDisplay(user, skills, seeksSkills)
-
+                    combine(userFlow, skillsFlow, seeksSkillsFlow, locationFlow) { user, skills, seeksSkills, location ->
+                        UiUserProfileDisplay(user, skills, seeksSkills, location)
+                    }.collect { combinedUser ->
+                            _currentUser.value = combinedUser
                     }
 
 
             } catch (exception: Exception) {
-                Log.d("ERROR123", "get user: ERROR")
+                Log.d("ERROR123", "get user current logged in user error")
                 _currentUser.value = null
             }
         }
