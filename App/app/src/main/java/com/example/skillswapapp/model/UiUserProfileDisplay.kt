8edcc1b6ffcd281703a49edaa6351c114d1a5fd4
KailanB <kailanbates@gmail.com>
2025-04-11 07:53:18 -0600

    added location data as separate entity for easier edit / delete

--- App/app/src/main/java/com/example/skillswapapp/model/UiUserProfileDisplay.kt
+++ App/app/src/main/java/com/example/skillswapapp/model/UiUserProfileDisplay.kt
@@ -1,5 +1,6 @@
 package com.example.skillswapapp.model
 
+import com.example.skillswapapp.data.entities.Location
 import com.example.skillswapapp.data.entities.User
 import com.example.skillswapapp.data.relations.UserSeeksSkillsDetails
 import com.example.skillswapapp.data.relations.UserSkillDetails
@@ -8,5 +9,6 @@ class UiUserProfileDisplay (
     val user : User,
 
     val skills: List<UserSkillDetails>,
-    val seeksSkills: List<UserSeeksSkillsDetails>
+    val seeksSkills: List<UserSeeksSkillsDetails>,
+    val location: Location
 )
