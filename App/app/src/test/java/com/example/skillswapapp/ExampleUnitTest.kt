559edbcb5b10d52e8cdef9e000a1a2017aa69c78
KailanB <kailanbates@gmail.com>
2025-04-11 07:58:45 -0600

    added a filter unit test

--- App/app/src/test/java/com/example/skillswapapp/ExampleUnitTest.kt
+++ App/app/src/test/java/com/example/skillswapapp/ExampleUnitTest.kt
@@ -1,5 +1,9 @@
 package com.example.skillswapapp
 
+import com.example.skillswapapp.data.relations.UserWithoutSecureInfo
+import com.example.skillswapapp.model.UiDisplaySkill
+import com.example.skillswapapp.model.UiUserDisplay
+import com.example.skillswapapp.view.filterUsersBySearchInput
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,6 +14,29 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @Test
+    fun testFilterUsersBySearchInput_withMatchingSkill() {
+        val users = listOf(
+            UiUserDisplay(
+                user = UserWithoutSecureInfo(1, "Alex Codewell", "alexcodewell@example.com", "Passionate coder and lifelong learner.", "Vancouver", "BC", null ),
+                skills = listOf(UiDisplaySkill(1, "C++", null)),
+                seeksSkills = listOf(UiDisplaySkill(1, "Yoga", null))
+            ),
+            UiUserDisplay(
+                user = UserWithoutSecureInfo(2, "Sky Forrest", "sky.forrest@example.com", "Living the plant parent life.", "Calgary", "AB", null ),
+                skills = listOf(UiDisplaySkill(3, "Herb Gardening", null)),
+                seeksSkills = listOf(UiDisplaySkill(1, "C++", null))
+            )
+        )
+
+        val searchInput = "C++"
+        val result = filterUsersBySearchInput(users, searchInput)
+
+        assertEquals(1, result.size)
+        assertEquals("Alex Codewell", result[0].user.name)
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
