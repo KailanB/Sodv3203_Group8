7511a2a6ae18bbe95d04b49fee5fdad3252df807
Deepanshi <deepanshi20ac@gmail.com>
2025-04-12 16:34:44 -0600

    kajal's part just include some classes

--- App/app/src/main/java/com/example/skillswapapp/data/database/AppDatabase.kt
+++ App/app/src/main/java/com/example/skillswapapp/data/database/AppDatabase.kt
@@ -5,11 +5,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.skillswapapp.data.dao.UserDao
+import com.example.skillswapapp.data.entities.Category
 import com.example.skillswapapp.data.entities.Location
+import com.example.skillswapapp.data.entities.Skill
 import com.example.skillswapapp.data.entities.User
+import com.example.skillswapapp.data.entities.UserSkills
 
 @Database(
-    entities = [User::class, Location::class],
+    entities = [User::class, Location::class, UserSkills::class, Skill::class, Category::class],
     version = 1,
     exportSchema = false
 )
@@ -27,7 +30,10 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "skillswap_database"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
                 INSTANCE = instance
                 instance
             }
