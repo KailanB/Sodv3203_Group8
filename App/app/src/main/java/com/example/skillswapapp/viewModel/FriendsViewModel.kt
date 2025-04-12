f8bcaf9d4953cdbfb2247bc3a9375960481de973
Deepanshi <deepanshi20ac@gmail.com>
2025-04-12 16:38:12 -0600

    this file is not in use

--- App/app/src/main/java/com/example/skillswapapp/viewModel/FriendsViewModel.kt
+++ App/app/src/main/java/com/example/skillswapapp/viewModel/FriendsViewModel.kt
@@ -7,33 +7,7 @@ import com.example.skillswapapp.data.relations.UserFriendList
 import com.example.skillswapapp.data.repository.iRepositories.FriendshipRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
-
+//not in use
 class FriendsViewModel(private val repository: FriendshipRepository) : ViewModel() {
 
-    // Expose a stream of all friends for a specific user
-    fun getAllFriends(userId: Int): Flow<List<UserFriendList>> {
-        return repository.getAllFriendshipsByIdStream(userId)
-    }
-
-    // Handle accepting a friend request
-    fun acceptFriendRequest(userId: Int, friendId: Int) {
-        viewModelScope.launch {
-            // Change the friendship status to 'accepted'
-            val friendship = Friendship(userId, friendId, "accepted")
-            repository.updateFriendship(friendship)
-        }
-    }
-
-    // Handle deleting a friend
-    fun deleteFriend(userId: Int, friendId: Int) {
-        viewModelScope.launch {
-            val friendship = Friendship(userId, friendId, "accepted")
-            repository.deleteFriendship(friendship)
-        }
-    }
-
-    // Get specific friendship by user ID
-    fun getFriendship(userId: Int): Flow<Friendship?> {
-        return repository.getFriendshipStream(userId)
-    }
 }
