d98c76d18d43bd8efeb56a58438ed61c409f2fd8
Deepanshi <deepanshi20ac@gmail.com>
2025-04-12 16:35:00 -0600

    include status

--- App/app/src/main/java/com/example/skillswapapp/data/relations/UserFriendList.kt
+++ App/app/src/main/java/com/example/skillswapapp/data/relations/UserFriendList.kt
@@ -4,5 +4,6 @@ data class UserFriendList(
     val user_id: Int,
     val name: String,
     val email: String,
-    val profile_intro: String?
+    val profile_intro: String?,
+    val status: String
 )
