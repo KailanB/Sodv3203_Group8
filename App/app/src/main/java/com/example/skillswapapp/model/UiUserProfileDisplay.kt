b15e5f82fcc0be0c06dcfca304d268d7d4ec271a
Deepanshi <deepanshi20ac@gmail.com>
2025-04-12 16:37:06 -0600

    include friend part

--- App/app/src/main/java/com/example/skillswapapp/model/UiUserProfileDisplay.kt
+++ App/app/src/main/java/com/example/skillswapapp/model/UiUserProfileDisplay.kt
@@ -2,13 +2,15 @@ package com.example.skillswapapp.model
 
 import com.example.skillswapapp.data.entities.Location
 import com.example.skillswapapp.data.entities.User
+import com.example.skillswapapp.data.relations.UserFriendList
 import com.example.skillswapapp.data.relations.UserSeeksSkillsDetails
 import com.example.skillswapapp.data.relations.UserSkillDetails
 
-class UiUserProfileDisplay (
-    val user : User,
+class UiUserProfileDisplay(
+    val user: User,
 
     val skills: List<UserSkillDetails>,
     val seeksSkills: List<UserSeeksSkillsDetails>,
-    val location: Location
+    val location: Location,
+    friends: List<UserFriendList>
 )
